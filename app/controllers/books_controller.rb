769eb3083d8e736bb5db52362dae9d160fdb7c7e
VentoPreto <67896322+ventopreto@users.noreply.github.com>
2024-11-04 23:16:07 -0300

    cria CRUD de books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+  before_action :set_authors, only: [:new, :create, :edit, :update]
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = t(".success")
+      redirect_to @book
+    else
+      flash[:danger] = t(".fail")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      flash[:success] = t(".success")
+      redirect_to [@book]
+    else
+      flash[:danger] = t(".fail")
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @book.rental
+      flash[:danger] = t(".fail")
+      redirect_to books_path
+    else
+      @book.destroy
+      flash[:success] = t(".success")
+      redirect_to books_path
+    end
+  end
+
+  private
+
+  def set_authors
+    @authors = Author.all
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :synopsis, :published_at, :author_id)
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  has_one :rental
   belongs_to :author
 
   validates :title, presence: true
